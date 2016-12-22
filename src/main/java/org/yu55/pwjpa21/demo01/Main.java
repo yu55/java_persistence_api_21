f2d80926ea993b759e50d2ff2e4a76449bcffd16
Marcin Pilaczynski <marcin.pil@gmail.com>
2016-12-22 22:13:20 +0100

    Bootstrap JPA with empty persistence unit.

--- src/main/java/org/yu55/pwjpa21/demo01/Main.java
+++ src/main/java/org/yu55/pwjpa21/demo01/Main.java
@@ -1,7 +1,16 @@
 package org.yu55.pwjpa21.demo01;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hi! :)");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo01-pu");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
     }
+
 }
