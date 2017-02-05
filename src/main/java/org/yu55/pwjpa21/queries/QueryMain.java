41cd3b368144d5d253d7d7a6b9f8536d2d4d8ad0
Marcin Pilaczynski <marcin.pil@gmail.com>
2017-02-05 13:02:09 +0100

    Simple select query.

---
+++ src/main/java/org/yu55/pwjpa21/queries/QueryMain.java
@@ -0,0 +1,20 @@
+package org.yu55.pwjpa21.queries;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class QueryMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("basics-pu-test");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        QueryService queryService = new QueryService(em);
+
+        queryService.queryCDs();
+    }
+
+}
--- src/main/java/org/yu55/pwjpa21/queries/QueryService.java
+++ src/main/java/org/yu55/pwjpa21/queries/QueryService.java
@@ -1,6 +1,27 @@
 package org.yu55.pwjpa21.queries;
 
-public class QueryService {
+import org.yu55.pwjpa21.basics.Journal;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
+class QueryService {
+
+    private EntityManager entityManager;
+
+    QueryService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    void queryCDs() {
+        Query query = entityManager.createQuery("SELECT j FROM Journal j");
+        List journals = query.getResultList();
+
+        for (int i=0; i<journals.size(); i++) {
+            Journal journal = (Journal)journals.get(i);
+            System.out.println(journal.getCds().size());
+            System.out.println(journal);
+        }
+    }
 }
